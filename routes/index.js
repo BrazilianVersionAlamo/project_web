6ad26d8a0f0e36b5d86609abd55d03e73adf490a
LUIZ EDUARDO DE MELO <luiz.melo@catolicasc.edu.br>
2019-08-28 21:40:53 -0300

    trabalho não finalizado

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var postsService = require ('../services/postsService');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var posts = postsService.getPosts{ };
+  res.render('index', { title: 'A Good Blog' });
 });
 
 module.exports = router;
---
+++ services/postsService.js
@@ -0,0 +1,23 @@
+
+var getPosts = function() {
+    var post = [
+        {
+            title: 'Post 1'
+            image: 'Post1.jpg'
+            description:'My first post'
+            body:'e o primeiro post que é meu'
+        },
+        {
+            title: 'Post 2'
+            image: 'Post2.jpg'
+            description:'My second post'
+            body:'e o segundo post que é meu'
+        },
+    ]
+
+    return posts;
+}
+
+module.export = {
+    getPosts: getPosts
+}
